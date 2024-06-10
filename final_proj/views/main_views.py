8bf62d1d7cabc1c8a7760229c154794064c54f1d
aoqlsdl <hyeeev@gmail.com>
2024-06-10 12:31:09 +0900

    feat: 로그인되지 않았을 경우 로그인페이지로 리다이렉트

--- final_proj/views/main_views.py
+++ final_proj/views/main_views.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template
-from final_proj.forms import RegisterForm
+from final_proj.views.auth_views import login_required
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
+@login_required # 데코레이터 적용
 def index():
     return render_template('index.html')
